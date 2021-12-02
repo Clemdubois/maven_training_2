41c2e99630c77c0acfb7f81c39c2e2b3d840fba5
Clément <45402873+Clemdubois@users.noreply.github.com>
2021-12-02 09:49:57 +0100

    Partie 3

---
+++ lernejo-tester-reactor/lernejo-tester/src/test/java/SomeLernejoTests.java
@@ -0,0 +1,20 @@
+import api.TestMethod;
+
+import java.lang.IllegalStateException;
+
+public class SomeLernejoTests {
+
+    @TestMethod
+    public void ok(){
+
+    }
+
+    @TestMethod
+    public void ko(){
+        throw new IllegalStateException();
+    }
+
+    public void none(){
+
+    }
+}
